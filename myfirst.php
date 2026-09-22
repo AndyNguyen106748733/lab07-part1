2df2e0f1032de723c21d586abdd492e401cd06b0
Andy Nguyen <106748733@student.swin.edu.au>
2026-09-22 20:46:54 +1000

    Version 0.0.1 | Set up foundations

---
+++ myfirst.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Using PHP variable, arrays and operators</title>
+</head>
+
+<body>
+    <h1>Using PHP variable, arrays and operators</h1>
+
+</body>
+
+</html>
